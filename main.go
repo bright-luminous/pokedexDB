de2f2d89719761852e55fca140096717b56e8507
bright-luminous <phisonkwan@gmail.com>
2022-06-13 16:30:39 +0700

    first commit for this branch

--- main.go
+++ main.go
@@ -1,38 +1,52 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"database/sql"
+	"fmt"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/bright-luminous/pokedexDB/graph"
-	"github.com/bright-luminous/pokedexDB/graph/generated"
 	"github.com/bright-luminous/pokedexDB/resource"
-	"github.com/go-chi/chi"
+	_ "github.com/lib/pq"
 )
 
-const defaultPort = "8080"
+const (
+	host     string = "localhost"
+	port     string = "5432"
+	user     string = "postgres"
+	password string = "Eauu0244"
+	dbName   string = "postgres"
+)
+
+// func main() {
+// 	r := chi.NewRouter()
+
+// 	operator, err := resource.NewPokemonSQLOperation("sql.DB")
+// 	resource.PrintIfErrorExist(err)
+
+// 	srv := handler.NewDefaultServer(
+// 		generated.NewExecutableSchema(
+// 			generated.Config{
+// 				Resolvers: &graph.Resolver{
+// 					DB: operator,
+// 				},
+// 			},
+// 		),
+// 	)
+
+// 	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 	r.Handle("/query", srv)
+
+// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+// 	log.Fatal(http.ListenAndServe(":"+defaultPort, r))
+// }
 
 func main() {
-	r := chi.NewRouter()
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+	db, err := sql.Open("postgres", psqlInfo)
+	resource.PrintIfErrorExist(err)
+	defer db.Close()
 
-	operator, err := resource.NewPokemonSQLOperation("sql.DB")
+	err = db.Ping()
 	resource.PrintIfErrorExist(err)
 
-	srv := handler.NewDefaultServer(
-		generated.NewExecutableSchema(
-			generated.Config{
-				Resolvers: &graph.Resolver{
-					DB: operator,
-				},
-			},
-		),
-	)
-
-	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	r.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
-	log.Fatal(http.ListenAndServe(":"+defaultPort, r))
+	fmt.Println("Successfully connected")
 }
